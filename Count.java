a5dc22ee8b177db9bab1962533901db3f1b5e272
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-04-26 13:28:44 -0400

    Mountain Ranges and Catalan Numbers
    A mountain range can be thought of as a sequence of upstrokes (/) and downstrokes (\) that:
    
    Starts and ends at ground level.
    
    Never goes below the ground level.
    
    Has exactly n upstrokes and n downstrokes.
    
     The number of such valid mountain ranges with n upstrokes and n downstrokes is the n-th Catalan number!

--- Count.java
+++ Count.java
@@ -10,8 +10,25 @@ public class Count {
         }
         return dp[n];
     }
+public static int mountainrange(int n){
+    int dp[] = new int [n+1];
+    dp[0] =1;
+    dp[1] =1;
+    for( int i=2; i<n+1;i++){
+        //for n pair how many mountain ranges are there  = Ci
+
+        for(int j=0; j<i; j++){
+            int inside = dp[j]; 
+            int outside = dp[i-j-1];
+            dp[i] +=inside* outside;
+
+        }
+    }
+    return dp[n];
+}
+
     public static void main(String[] args) {
-        int n = 3;
-        System.out.println("Number of BSTs with " + n + " nodes: " + countBst(n));
+        int n = 4;
+        System.out.println("Number of BSTs with " + n + " nodes: " + mountainrange(n));
     }
 }
